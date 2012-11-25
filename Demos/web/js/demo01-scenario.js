6bc8cbed47ab679b8098ea70af43a1a2386c4792
Ryan Catlin <ryan.catlin@gmail.com>
2012-11-25 00:58:54 -0800

    Refactor scenario definition file

--- Demos/web/js/demo01-scenario.js
+++ Demos/web/js/demo01-scenario.js
@@ -7,39 +7,41 @@ var spinner = new Spinner({
 var scenario = new Scenario
 scenario.set('Example Scenario', 'active')
 firstFloor = scenario.addFloor('First Floor', 0);
-secondFloor = scenario.addFloor('Second Floor', 1);
-thirdFloor = scenario.addFloor('Third Floor', 2);
-// firstFloor = scenario.getFloor(0)
-// Professor's Office
-newRoom = firstFloor.addRoom('5thhall', 'SW 5th & Hall', 0, 0, 0)
-newRoom.addWall('E 5th Hall', 'e', 'A-east.jpg')
-newRoom.addWall('W 5th Hall', 'w', 'A-west.jpg')
-newRoom.addWall('N 5th Hall', 'n', 'A-north.jpg')
-newRoom.addWall('S 5th Hall', 's', 'A-south.jpg')
-// Hallway 0
-newRoom = firstFloor.addRoom('north5th', 'Upper SW 5th & Hall', 0, 1, 0)
-newRoom.addWall('E Upper 5th Hall', 'e', 'B-east.jpg')
-newRoom.addWall('W Upper 5th Hall', 'w', 'B-west.jpg')
-newRoom.addWall('N Upper 5th Hall', 'n', 'B-north.jpg')
-newRoom.addWall('S Upper 5th Hall', 's', 'B-south.jpg')
-// Hallway 1
-newRoom = firstFloor.addRoom('westhall', 'West on Hall', 1, 0, 0)
-newRoom.addWall('E West on Hall', 'e', 'C-east.jpg')
-newRoom.addWall('W West on Hall', 'w', 'C-west.jpg')
-newRoom.addWall('N West on Hall', 'n', 'C-north.jpg')
-newRoom.addWall('S West on Hall', 's', 'C-south.jpg')
-// Hallway 2
-newRoom = firstFloor.addRoom('westhall2', 'West on Hall 2', 2, 0, 0)
-newRoom.addWall('E West on Hall 2', 'e', 'D-east.jpg')
-newRoom.addWall('W West on Hall 2', 'w', 'D-west.jpg')
-newRoom.addWall('N West on Hall 2', 'n', 'D-north.jpg')
-newRoom.addWall('S West on Hall 2', 's', 'D-south.jpg')
-// Other Office
-newRoom = firstFloor.addRoom('SW 4th & Hall', 'SW 4th & Hall', 2, 0, 0)
-newRoom.addWall('E 4th Hall', 'e', 'E-east.jpg')
-newRoom.addWall('W 4th Hall', 'w', 'E-west.jpg')
-newRoom.addWall('N 4th Hall', 'n', 'E-north.jpg')
-newRoom.addWall('S 4th Hall', 's', 'E-south.jpg')
+
+if (newRoom = firstFloor.addRoom('5thhall', 'SW 5th & Hall', 0, 0, 0)) {
+        newRoom.addWall('E 5th Hall', 'e', 'A-east.jpg')
+        newRoom.addWall('W 5th Hall', 'w', 'A-west.jpg')
+        newRoom.addWall('N 5th Hall', 'n', 'A-north.jpg')
+        newRoom.addWall('S 5th Hall', 's', 'A-south.jpg')
+}
+
+if (newRoom = firstFloor.addRoom('north5th', 'Upper SW 5th & Hall', 0, 1, 0)) {
+        newRoom.addWall('E Upper 5th Hall', 'e', 'B-east.jpg')
+        newRoom.addWall('W Upper 5th Hall', 'w', 'B-west.jpg')
+        newRoom.addWall('N Upper 5th Hall', 'n', 'B-north.jpg')
+        newRoom.addWall('S Upper 5th Hall', 's', 'B-south.jpg')
+}
+
+if (newRoom = firstFloor.addRoom('westhall', 'West on Hall', 1, 0, 0)) {
+        newRoom.addWall('E West on Hall', 'e', 'C-east.jpg')
+        newRoom.addWall('W West on Hall', 'w', 'C-west.jpg')
+        newRoom.addWall('N West on Hall', 'n', 'C-north.jpg')
+        newRoom.addWall('S West on Hall', 's', 'C-south.jpg')
+}
+
+if (newRoom = firstFloor.addRoom('westhall2', 'West on Hall 2', 2, 0, 0)) {
+        newRoom.addWall('E West on Hall 2', 'e', 'D-east.jpg')
+        newRoom.addWall('W West on Hall 2', 'w', 'D-west.jpg')
+        newRoom.addWall('N West on Hall 2', 'n', 'D-north.jpg')
+        newRoom.addWall('S West on Hall 2', 's', 'D-south.jpg')
+}
+
+if (newRoom = firstFloor.addRoom('SW 4th & Hall', 'SW 4th & Hall', 3, 0, 0)) {
+        newRoom.addWall('E 4th Hall', 'e', 'E-east.jpg')
+        newRoom.addWall('W 4th Hall', 'w', 'E-west.jpg')
+        newRoom.addWall('N 4th Hall', 'n', 'E-north.jpg')
+        newRoom.addWall('S 4th Hall', 's', 'E-south.jpg')
+}
 // Elevator - first floor
 // newRoom = firstFloor.addRoom('elevator', 'Elevator', 1, 3, 0)
 // newRoom.addWall('E Elevator Wall', 'e', 'elev-e.jpg');
