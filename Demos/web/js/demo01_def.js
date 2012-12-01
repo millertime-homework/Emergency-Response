f8e7df4bcf4427d2078603c8e56a30da14219895
Ryan Catlin <ryan.catlin@gmail.com>
2012-11-30 22:03:19 -0800

    Load scenario from json array

---
+++ Demos/web/js/demo01_def.js
@@ -0,0 +1,156 @@
+var scenarioDef = {
+        'name': 'Demo 01 Scenario',
+        '_floors': {
+                'First Floor': {
+                        'z': 0,
+                        '_rooms': {
+                                'SW 5th & Hall': {
+                                        'id': '5thhall', 
+                                        'x': 0, 
+                                        'y': 0, 
+                                        '_walls': {
+                                                'e': {
+                                                        'name': 'E 5th Hall', 
+                                                        'image': 'A-east.jpg',
+                                                },
+                                                'w': {
+                                                        'name': 'W 5th Hall',
+                                                        'image': 'A-west.jpg',
+                                                
+                                                },
+                                                'n': {
+                                                        'name': 'N 5th Hall',   
+                                                        'image': 'A-north.jpg',
+                                                },
+                                                's': {
+                                                        'name': 'S 5th Hall', 
+                                                        'image': 'A-south.jpg',
+                                                },
+                                        },     
+                                },
+                                'Upper SW 5th & Hall': {
+                                        'name': 'north5th',
+                                        'x': 0,
+                                        'y': 1,
+                                        '_walls': {
+                                                'e':  {
+                                                        'name': 'E Upper 5th Hall', 
+                                                        'image': 'B-east.jpg',
+                                                }, 
+                                                'w': {
+                                                        'name': 'W Upper 5th Hall', 
+                                                        'image': 'B-west.jpg',
+                                                },
+                                                'n': {
+                                                        'name': 'N Upper 5th Hall', 
+                                                        'image': 'B-north.jpg',
+                                                },
+                                                's': {
+                                                        'name': 'S Upper 5th Hall', 
+                                                        'image': 'B-south.jpg',
+                                                },
+                                        },
+                                },
+                                'West on Hall': {
+                                        'name': 'westhall',
+                                        'x': 1,
+                                        'y': 0,
+                                        '_walls': {
+                                                'e': {
+                                                        'name': 'E West on Hall',
+                                                        'image': 'C-east.jpg',
+                                                },
+                                                'w': {
+                                                        'name': 'W West on Hall',
+                                                        'image': 'C-west.jpg',
+                                                },
+                                                'n': {
+                                                        'name': 'N West on Hall',
+                                                        'image': 'C-north.jpg',
+                                                },
+                                                's': {
+                                                        'name': 'S West on Hall',
+                                                        'image': 'C-south.jpg',
+                                                },
+                                        },
+                                },
+                                'West on Hall 2': {
+                                        'name': 'westhall2',
+                                        'x': 2,
+                                        'y': 0,
+                                        '_walls': {
+                                                'e': {
+                                                        'name': 'E West on Hall 2',
+                                                        'image': 'D-east.jpg',
+                                                },
+                                                'w': {
+                                                        'name': 'W West on Hall 2',
+                                                        'image': 'D-west.jpg',
+                                                },
+                                                'n': {
+                                                        'name': 'N West on Hall 2',
+                                                        'image': 'D-north.jpg',
+                                                },
+                                                's': {
+                                                        'name': 'S West on Hall 2',
+                                                        'image': 'D-south.jpg',
+                                                },
+                                        },
+                                },
+                                'SW 4th & Hall': {
+                                        'name': '4thhall',
+                                        'x': 3,
+                                        'y': 0,
+                                        '_walls': {
+                                                'e': {
+                                                        'name': 'E 4th Hall',
+                                                        'image': 'E-east.jpg',
+                                                },
+                                                'w': {
+                                                        'name': 'W 4th Hall',
+                                                        'image': 'E-west.jpg',
+                                                },
+                                                'n': {
+                                                        'name': 'N 4th Hall',
+                                                        'image': 'E-north.jpg',
+                                                },
+                                                's': {
+                                                        'name': 'S 4th Hall',
+                                                        'image': 'E-south.jpg',
+                                                },
+                                        },
+                                },
+                        },
+                },
+        },
+        '_player': {
+                'x': 0,
+                'y': 0,
+                'z': 0,
+                '_facing': 'n'
+        },
+}
+
+// '': {
+//         'name': '',
+//         'x': ,
+//         'y': ,
+//         '_walls': {
+//                 'e': {
+//                         'name': '',
+//                         'image': '',
+//                 },
+//                 'w': {
+//                         'name': '',
+//                         'image': '',
+//                 },
+//                 'n': {
+//                         'name': '',
+//                         'image': '',
+//                 },
+//                 's': {
+//                         'name': '',
+//                         'image': '',
+//                 },
+//         },
+// },
