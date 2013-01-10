2e47ec6537947ae8fae61733bd4e0963eb1a3119
chri dyson <chrid@pdx.edu>
2013-01-09 17:44:07 -0800

    Add a try/catch handler around the key handler, so if there is an error, it does not break everything

--- web/js/keyhandler.js
+++ web/js/keyhandler.js
@@ -10,33 +10,37 @@ jQuery(document).ready(function($) {
             return true;
         }
         keypressed = true;
-        if (event.which == 37) {
-            // left arrow key
-            event.preventDefault();
-            $(document).trigger('player-move', 'turn-left');
-        } else if (event.which == 38) {
-            // up arrow key
-            event.preventDefault();
-            $(document).trigger('player-move', 'move-forward');
-        } else if (event.which == 39) {
-            // right arrow key
-            event.preventDefault();
-            $(document).trigger('player-move', 'turn-right');
-        } else if (event.which == 40) {
-            // down arrow key
-            event.preventDefault();
-            $(document).trigger('player-move', 'move-down')
-        } else if (event.which == 69) {
-            // start earthquake
-            event.preventDefault();
-            $(document).trigger('startEarthquake')
-        } else if(event.which == 27){
-            // pause game
-            playerState = "Paused";
-            evalGameState();
-        } else {
-            // Debugging purposes :-/
-            // alert(event.which);
+        try {
+            if (event.which == 37) {
+                // left arrow key
+                event.preventDefault();
+                $(document).trigger('player-move', 'turn-left');
+            } else if (event.which == 38) {
+                // up arrow key
+                event.preventDefault();
+                $(document).trigger('player-move', 'move-forward');
+            } else if (event.which == 39) {
+                // right arrow key
+                event.preventDefault();
+                $(document).trigger('player-move', 'turn-right');
+            } else if (event.which == 40) {
+                // down arrow key
+                event.preventDefault();
+                $(document).trigger('player-move', 'move-down')
+            } else if (event.which == 69) {
+                // start earthquake
+                event.preventDefault();
+                $(document).trigger('startEarthquake')
+            } else if(event.which == 27){
+                // pause game
+                playerState = "Paused";
+                evalGameState();
+            } else {
+                // Debugging purposes :-/
+                // alert(event.which);
+            }
+        } catch(e) {
+            console.log(e);
         }
         keypressed = false;
 
