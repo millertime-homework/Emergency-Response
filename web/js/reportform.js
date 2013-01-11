ad1853009c58cad953b668102cf3c8f1f615e1d2
Russell <compsciwizkid@gmail.com>
2013-01-11 02:25:13 -0800

    fixed unexpected end of input js errors

--- web/js/reportform.js
+++ web/js/reportform.js
@@ -1,10 +1,8 @@
 jQuery(document).ready(function($){
-
 	$('#submit-button').click(function()
 	{
 		// Send email function goes here.
 		('#report-modal').hide();
 		('main-menu').show();
-	})
-
-}
+	});
+});
