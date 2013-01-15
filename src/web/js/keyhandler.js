e22da276ff79f4e4269250809de2b20470b1bab2
scott <scott.d.palomino@gmail.com>
2013-01-15 11:17:43 -0800

    Bug #2617 - Fix for KeyEvents failing to fire in IE.

--- src/web/js/keyhandler.js
+++ src/web/js/keyhandler.js
@@ -4,51 +4,51 @@ var allowKeyEvents = false; //Will be enabled/disabled by methods in functions.j
 /* ######################################## */
 
 jQuery(document).ready(function ($) {
-    // Key is pressed
-    $(window).keydown(function (event) {
+    // Key is pressed.
+    $(document).keydown(function (event) {
         if (keypressed) {
             return true;
         }
-        if (allowKeyEvents) {
-            var keyEvent = null;
-            var keyEventArg = null;
-            keypressed = true;
-            try {
-                if (event.which == 37) {
+        var keyEvent = null;
+        var keyEventArg = null;
+        keypressed = true;
+        try {
+            //These are key events that should only be triggered when the game is in the correct state.
+            if (allowKeyEvents) {
+                if (event.keyCode == 37) {
                     // left arrow key
                     keyEvent = 'player-move';
                     keyEventArg = 'turn-left';
-                } else if (event.which == 38) {
+                } else if (event.keyCode == 38) {
                     // up arrow key
                     keyEvent = 'player-move';
                     keyEventArg = 'move-forward';
-                } else if (event.which == 39) {
+                } else if (event.keyCode == 39) {
                     // right arrow key
                     keyEvent = 'player-move';
                     keyEventArg = 'turn-right';
-                } else if (event.which == 40) {
+                } else if (event.keyCode == 40) {
                     // down arrow key
                     keyEvent = 'player-move';
                     keyEventArg = 'move-down';
-                } else if (event.which == 69) {
+                } else if (event.keyCode == 69) {
                     // start earthquake
                     keyEvent = 'startEarthquake';
-                } else if (event.which == 27) {
+                }
+            }
+            //The remaining events can be triggered at all times.
+            if (event.keyCode == 27) {
                     // pause game
                     playerState = "Paused";
                     evalGameState();
-                } else {
-                    // Debugging purposes :-/
-                    // alert(event.which);
-                }
-                if (keyEvent) {
-                    event.preventDefault();
-                    $(document).trigger(keyEvent, keyEventArg);
-                }
-            } catch (e) {
-                console.log(e);
             }
-            keypressed = false;
+            if (keyEvent) {
+                event.preventDefault();
+                $(document).trigger(keyEvent, keyEventArg);
+            }
+        } catch (e) {
+            console.log(e);
         }
+        keypressed = false;
     })
 })
