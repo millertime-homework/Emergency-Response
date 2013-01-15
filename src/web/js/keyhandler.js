9a0a4087b3293f1bcabb18df34bdf922d01f94af
scott <scott.d.palomino@gmail.com>
2013-01-15 10:45:00 -0800

    Bug #2550 - Minor change to keyhandler logic to eliminate unecessary code execution when key events are not allowed.

--- src/web/js/keyhandler.js
+++ src/web/js/keyhandler.js
@@ -9,45 +9,46 @@ jQuery(document).ready(function ($) {
         if (keypressed) {
             return true;
         }
-        var keyEvent = null;
-        var keyEventArg = null;
-        keypressed = true;
-        try {
-            if (event.which == 37) {
-                // left arrow key
-                keyEvent = 'player-move';
-                keyEventArg = 'turn-left';
-            } else if (event.which == 38) {
-                // up arrow key
-                keyEvent = 'player-move';
-                keyEventArg = 'move-forward';
-            } else if (event.which == 39) {
-                // right arrow key
-                keyEvent = 'player-move';
-                keyEventArg = 'turn-right';
-            } else if (event.which == 40) {
-                // down arrow key
-                keyEvent = 'player-move';
-                keyEventArg = 'move-down';
-            } else if (event.which == 69) {
-                // start earthquake
-                keyEvent = 'startEarthquake';
-            } else if (event.which == 27) {
-                // pause game
-                playerState = "Paused";
-                evalGameState();
-            } else {
-                // Debugging purposes :-/
-                // alert(event.which);
+        if (allowKeyEvents) {
+            var keyEvent = null;
+            var keyEventArg = null;
+            keypressed = true;
+            try {
+                if (event.which == 37) {
+                    // left arrow key
+                    keyEvent = 'player-move';
+                    keyEventArg = 'turn-left';
+                } else if (event.which == 38) {
+                    // up arrow key
+                    keyEvent = 'player-move';
+                    keyEventArg = 'move-forward';
+                } else if (event.which == 39) {
+                    // right arrow key
+                    keyEvent = 'player-move';
+                    keyEventArg = 'turn-right';
+                } else if (event.which == 40) {
+                    // down arrow key
+                    keyEvent = 'player-move';
+                    keyEventArg = 'move-down';
+                } else if (event.which == 69) {
+                    // start earthquake
+                    keyEvent = 'startEarthquake';
+                } else if (event.which == 27) {
+                    // pause game
+                    playerState = "Paused";
+                    evalGameState();
+                } else {
+                    // Debugging purposes :-/
+                    // alert(event.which);
+                }
+                if (keyEvent) {
+                    event.preventDefault();
+                    $(document).trigger(keyEvent, keyEventArg);
+                }
+            } catch (e) {
+                console.log(e);
             }
-            if (keyEvent && allowKeyEvents) {
-                event.preventDefault();
-                $(document).trigger(keyEvent, keyEventArg);
-            }
-        } catch (e) {
-            console.log(e);
+            keypressed = false;
         }
-        keypressed = false;
-
     })
 })
