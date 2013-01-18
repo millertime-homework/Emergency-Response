86599449f9923555d9dbf900e970af13bac43c7a
Russell <compsciwizkid@gmail.com>
2013-01-18 07:34:08 -0800

    activated the help button

---
+++ src/web/js/help.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($){
+    $('#help').click(function() {
+        displayModal('Help', 'Up Arrow: Move Forward<br />Left Arrow: Turn Left<br />Right Arrow: Turn Right', null);
+    });
+});
