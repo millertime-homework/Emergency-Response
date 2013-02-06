597c445c7e65dcb52dda2f5dad8add2cfe344ad0
scott <scott.d.palomino@gmail.com>
2013-02-05 21:57:21 -0800

    Fix for objectives failing to complete in IE

--- src/web/js/functions.js
+++ src/web/js/functions.js
@@ -368,7 +368,7 @@ function setObjective(name, displayText) {
 };
 
 function completeObjective(name) {
-    objective = scenario.objectives.inProgress[name];
+    var objective = scenario.objectives.inProgress[name];
     if (objective) {
         scenario.objectives.completed[name] = objective;
         delete scenario.objectives.inProgress[name];
@@ -377,7 +377,7 @@ function completeObjective(name) {
 };
 
 function failObjective(name) {
-    objective = scenario.objectives.inProgress[name];
+    var objective = scenario.objectives.inProgress[name];
     if (objective) {
         scenario.objectives.failed[name] = objective;
         delete scenario.objectives.inProgress[name];
