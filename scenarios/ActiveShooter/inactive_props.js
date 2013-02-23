6f80bb921dc5fa546deacac70f8bd647c2dea1f9
Russell <compsciwizkid@gmail.com>
2013-02-23 13:06:21 -0800

    forgot the inactive props...

--- scenarios/ActiveShooter/inactive_props.js
+++ scenarios/ActiveShooter/inactive_props.js
@@ -1,3 +1,6 @@
+'inactiveProps': ['UseDoor', 'policeman', 'crowbar', 'shooter', 'shooterR111',
+                  'shooter-downed', 'HidingPlace', 'doorShadeDown', 
+                  'hidingSpot', 'hidingSpot2', 'doorHandleDone', 'lightSwitchDone'],
 '_player': {
     'x': 2,
     'y': 0,
