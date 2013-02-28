2c0f3eb56ee35844390267b229a78807989282f9
Ryan Catlin <ryan.catlin@gmail.com>
2013-02-27 19:31:18 -0800

    Add showConversation to events

--- src/web/js/events.js
+++ src/web/js/events.js
@@ -116,4 +116,8 @@ jQuery(document).ready(function($) {
     $(document).on('failObjective', function (event, name) {
         failObjective(name);
     });
+
+    $(document).on('showConversation', function (event, conversationName) {
+        showConversation(conversationName);
+    });
 });
