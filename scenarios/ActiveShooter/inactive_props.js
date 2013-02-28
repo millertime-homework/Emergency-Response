d12ab5ce27cce244cc95b0b6fc457f81a5b65639
John Wehland <johnwehland@gmail.com>
2013-02-27 17:44:44 -0800

    These are all the changes I made in first-hinding-people-adds. There was some sort of error in the pull request. Also I've added some of the functionality for when the 'shooter is close and the player runs' that was lost when the 2nd floor was added. Finally I also added some new content for when the shooter is far away. This new content is a bit experimental.

--- scenarios/ActiveShooter/inactive_props.js
+++ scenarios/ActiveShooter/inactive_props.js
@@ -1,11 +1,11 @@
 'inactiveProps': ['UseDoor', 'policeman', 'crowbar', 'shooter', 'shooterR111',
                   'shooter-downed', 'HidingPlace', 'doorShadeDown', 
                   'hidingSpot', 'hidingSpot2', 'doorHandleDone', 'lightSwitchDone',
-                  'hidingStudent1', 'hidingStudent2', 'hidingStudent3'],
+                  'hidingStudent1', 'hidingStudent2', 'hidingStudent3', 'shooter220S'],
 '_player': {
     'x': 2,
     'y': 0,
     'z': 0,
     '_facing': 'n',
-    'inventory': []
+    'inventory': [{'name':'Lab Work', 'image':'labwork.png', 'width':40, 'height':60}]
 }
--- scenarios/ActiveShooter/triggers/getToFrontDoors.js
+++ scenarios/ActiveShooter/triggers/getToFrontDoors.js
@@ -1,8 +1,8 @@
 'getToFrontDoors' : {
     'events': {
-        'addToScene': ['UseDoor', 'hidingSpot', 'hidingSpot2'],
+        'addToScene': ['UseDoor', 'hidingSpot', 'hidingSpot2', 'shooter220S'],
         'setObjective': ['getToFrontDoors', 'Go to the front doors of the school and get out of the building'],
         'showModal': ['[Intercom]', 'Alert! There is a gunman in the mall next to the school.']
     },
-    'enableTriggers' : ['FrontDoorsReached']
+    'enableTriggers' : ['getShotAt', 'FrontDoorsReached', 'shotInStairwell']
 }
