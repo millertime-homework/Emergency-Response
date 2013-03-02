00a7842f776a1813fc7ffb76ee54255260bf49b3
John Wehland <johnwehland@gmail.com>
2013-03-01 17:49:44 -0800

    hide-bubble

--- scenarios/ActiveShooter/triggers/failedToTurnOffLights.js
+++ scenarios/ActiveShooter/triggers/failedToTurnOffLights.js
@@ -1,6 +1,6 @@
 'failedToTurnOffLights' : {
     'disabled' : true,
     'events' : {
-        'endGame': ['Game Over', "Although you took action and attempted to hide, because you did not turn off the lights, the room caught the eye of the shooter, who kicked down the door and stormed into the room. You were critically wounded."]
+        'showModal': ["The shooter noticed that the lights were on in the classroom and tried to shoot the lock off the door! Fortunately for you, this did not work. Unfortunately some of his bullets missed the door and went through the classroom walls injuring one of the students! Shouting explatives the shooter gave up and walked away to find easier targets. The student is bleeding from the arm, but should be ok for a few hours. That was a very close call! Next time conceal your position better by turning off the lights."]
     }
 }
