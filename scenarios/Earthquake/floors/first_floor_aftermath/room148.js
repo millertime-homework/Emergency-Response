507895401229052d463d26c2494773aa290ab41d
rf11707@gmail.com <rf11707@gmail.com>
2013-03-02 12:27:07 -0800

    Fixed Mrs. Wheelchair.  I moved the props to the correct location

--- scenarios/Earthquake/floors/first_floor_aftermath/room148.js
+++ scenarios/Earthquake/floors/first_floor_aftermath/room148.js
@@ -8,6 +8,32 @@
             'image': 'R140-east.jpg',
             'destination': {
                 'x': 2
+            },
+			'_props': {
+                'mrs-wheelchair': {
+                    'name': 'mrs_wheelchair',
+                    'image': 'mrs_wheelchair.png',
+                    'width': 150,
+                    'height': 150,
+                    'left': 600,
+                    'top': 350,
+                    'action': 'showConversation',
+                    'actionVariables': {
+                        'conversationName': 'Mrs Lydia'
+                    }
+                },
+                'wheelchair':{
+                    'name':'the_wheelchair',
+                    'image':'wheelchair.png',
+                    'width':150,
+                    'height':150,
+                    'left': 450,
+                    'top': 300,
+                    'action':'showConversation',
+                    'actionVariables': {
+                        'conversationName' : 'wheelchair'
+                    }
+                }
             }
         },
         'w': {
@@ -15,7 +41,7 @@
             'image': 'R140-west.jpg',
             'destination': {
                 'x': 0
-            }
+            },
         },
         'n': {
             'name': 'NHall148',
