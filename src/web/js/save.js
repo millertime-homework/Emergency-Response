0db96f7cb732c8fb684cf6ea2b1476ddbbfa9436
chri dyson <chrid@pdx.edu>
2013-03-09 20:18:08 -0800

    Restore the darkness (though the flashlight turning back on does not seem to work right (actually it probably just hadn't saved after I turned it on))

--- src/web/js/save.js
+++ src/web/js/save.js
@@ -1,4 +1,4 @@
-var saveableVars = {'x':0,'y':1,'z':2,'facing':3,'scenario':4,'objectives':5,'inactiveProps':6,'inventory':7,'triggers':8};
+var saveableVars = {'x':0,'y':1,'z':2,'facing':3,'scenario':4,'objectives':5,'inactiveProps':6,'inventory':7,'lightsOn':8,'triggers':9};
 var triggerTypes = ['disabled', 'pool', 'waitingForSignal', 'deferredByMoves', 'deferredByTime', 'deferredByObjectives'];
 function saveGame() {
     if (gameState == GAME_STATE_OVER) {
@@ -9,12 +9,14 @@ function saveGame() {
     for (var i in scenario.inactiveProps) 
         if (scenario.inactiveProps.hasOwnProperty(i))
             inactList[inactList.length] = i;
+    var flashlightOverlay = jQuery('#flashlight-overlay');
     var saveable = [
         /*"x":*/ player.x, /*"y":*/ player.y, /*"z":*/ player.z, /*"facing":*/ player.facing,
         /*"scenario":*/ currentScenario,
         /*"objectives":*/ scenario.objectives,
         /*"inactiveProps":*/ inactList,
-        /*"inventory":*/ player.inventory.items
+        /*"inventory":*/ player.inventory.items,
+        /*"lightsOn":*/ lightsOn ? 'on' : flashlightOverlay.hasClass('flashlight-on')
     ];
     for (var j = 0; j < triggerTypes.length; j++) {
         var name = triggerTypes[j];
@@ -70,6 +72,17 @@ function loadGame() {
     scenario.inactiveProps = {};
     for (var i = 0; i < saveable[saveableVars.inactiveProps].length; i++)
         scenario.inactiveProps[saveable[saveableVars.inactiveProps][i]] = true;
+    var flashlightOverlay = jQuery('#flashlight-overlay');
+    if(saveable[saveableVars.lightsOn] != 'on') {
+        jQuery(document).trigger('disableLights');
+        if(saveable[saveableVars.lightsOn]) { // flashlights on
+            flashlightOverlay.removeClass('flashlight-off');
+            flashlightOverlay.addClass('flashlight-on');
+        } else {
+            flashlightOverlay.removeClass('flashlight-on');
+            flashlightOverlay.addClass('flashlight-off');
+        }
+    }
     renderScene();
     updateMap();
     jQuery('#objective ul').empty();
