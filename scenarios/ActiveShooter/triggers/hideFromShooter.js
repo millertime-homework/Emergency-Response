a7d4db3280d7e112c46c6c4599f51728e50364a1
John Wehland <johnwehland@gmail.com>
2013-03-09 17:03:41 -0800

    These are the triggers and changes needed for the NW office in the library

--- scenarios/ActiveShooter/triggers/hideFromShooter.js
+++ scenarios/ActiveShooter/triggers/hideFromShooter.js
@@ -13,4 +13,18 @@
                         'shooterCloseGetShot', 'chanceofEscape', 'abort', 'shooterEnters011'],
     
     'startTriggers' : ['failedToLockTheDoor', 'hiddenFromShooter']
+},
+
+'hideFromShooter_041' : {
+    'events' : {
+        'setObjectives' : [['lockTheDoor041', 'Prevent the shooter from getting into the room!', 'turnOffLights041', 'Turn off the lights in the NW room of the library.',
+                            'pullDownWindowShade041', 'Pull down the window shades in the NW Room of the library', 'hideByDoor041', 'Hide by the door in the NW Room of the library']],
+                            
+        'showModal': ['You hear gunshots! They are close by, somehwere in the library! It looks like the door to this room might lock!']
+    },
+    'enableTriggers' : ['turnedOffLights041', 'pulledDownShade041', 'hiddenByDoor041', 'lockedTheDoor041', 'failedToHide041',
+                        'failedToTurnOffLights041', 'failedToPullDownShade041', 'failedToHideByDoor041', 'failedToLockTheDoor041',
+                        'shooterCloseGetShot041', 'shooterEnters041'],
+    
+    'startTriggers' : ['failedToLockTheDoor041', 'hiddenFromShooter041', 'hiddenFromShooter041']
 }
