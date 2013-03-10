b62086ce9d66c3b9228a9830fc04bea0f35e653d
Russell <compsciwizkid@gmail.com>
2013-03-10 13:06:39 -0700

    remove unused crowbar trigger

--- scenarios/ActiveShooter/triggers/takeCrowbar.js
+++
@@ -1,8 +0,0 @@
-'takeCrowbar': {
-    'events': {
-        'takeFromScene': [ {'name': 'crowbar', 'image': 'crowbar.png', 'width':32, 'height':20},
-                           'crowbar'],
-        'completeObjective': ['getWeapon'],
-        'setObjective': ['attackShooter', 'Find and attack the shooter']
-    }
-}
