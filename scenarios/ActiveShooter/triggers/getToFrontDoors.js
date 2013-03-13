bce953a80ec92141163e0a32c5ea3450ea79c105
John Wehland <johnwehland@gmail.com>
2013-03-12 22:29:14 -0700

    fixed shooters width in all images

--- scenarios/ActiveShooter/triggers/getToFrontDoors.js
+++ scenarios/ActiveShooter/triggers/getToFrontDoors.js
@@ -4,5 +4,5 @@
         'setObjective': ['getToFrontDoors', 'Choose what to do: Run Hide or Fight'],
         'showModal': ['[Intercom]', 'Alert! There is a gunman in the mall next to the school.']
     },
-    'startRandomTrigger': ['pathNotClear', 'pathClear']
+    'startRandomTrigger': ['pathNotClear']//, 'pathClear']
 }
