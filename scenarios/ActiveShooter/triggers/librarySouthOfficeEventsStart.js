a5947c1f69b924f79136deda2e80e1493a1c07a3
John Wehland <johnwehland@gmail.com>
2013-03-13 10:52:13 -0700

    fixed merge conflicts'

--- scenarios/ActiveShooter/triggers/librarySouthOfficeEventsStart.js
+++ scenarios/ActiveShooter/triggers/librarySouthOfficeEventsStart.js
@@ -4,7 +4,7 @@
     'events': {
         'setObjectives' : [['findWeapon', 'Find a weapon', 
                             'studentsGatherWeapons031', 'Get students to gather weapons in room 031']],
-        'showConversation' : ['Suzie', null, true, false]
+        'showConversation' : ['Suzie', 1, true, false]
     },
     'enableTriggers' : ['shooterCloseGetShot']
 }
