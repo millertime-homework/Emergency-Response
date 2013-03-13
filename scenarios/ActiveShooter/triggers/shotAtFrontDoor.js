8bb9d7a16171110ca10913ba3a4cda344eb638ba
Russell <compsciwizkid@gmail.com>
2013-03-13 03:30:42 -0700

    switched active shooter endings to cutscenes

--- scenarios/ActiveShooter/triggers/shotAtFrontDoor.js
+++
@@ -1,5 +0,0 @@
-'shotAtFrontDoor': {
-    'events': {
-        'endGame': ['Game Over','You desperately try to get the door open, but it is too late. The shooter comes around the corner and sees you out in the open. He shoots you [Game Over]']
-    },
-}
