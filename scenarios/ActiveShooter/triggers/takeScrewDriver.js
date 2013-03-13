13e1cfd73025532a01fc6f87ab1469befc3628a4
Russell <compsciwizkid@gmail.com>
2013-03-12 20:01:59 -0700

    taking screwdriver now only initiates the fight if the player has reached that objective

--- scenarios/ActiveShooter/triggers/takeScrewDriver.js
+++ scenarios/ActiveShooter/triggers/takeScrewDriver.js
@@ -2,9 +2,6 @@
     'events': {
         'addPoints' : [25],
         'takeFromScene' : [ {'name': 'Screwdriver', 'image': 'screwdriver.png', 'width':100, 'height':66 },
-                          'screwdriver031' ],
-        'completeObjective' : ['findWeapon'],
-        'showConversation' : ['shooterEntersRoom', null, true, true]
-    },
-    'abortTriggers' : ['shooterApproaches031']
+                          'screwdriver031' ]
+    }
 }
