5b3a3bb52cfab37f96f0430f61fa2a4e63f5fcaf
chri dyson <chrid@pdx.edu>
2013-03-13 14:27:43 -0700

    Generate a code from a representative sample

--- other/Huffman.hs
+++ other/Huffman.hs
@@ -4,6 +4,7 @@ import PriorityQueue
 import Data.Map ((!),Map)
 import qualified Data.Map as Map
 import qualified Data.ByteString as ByteString
+import Data.List
 
 data (Real n) => HTree n t = HLeaf n t | HBranch n (HTree n t) (HTree n t) deriving (Show,Eq)
 
@@ -30,3 +31,20 @@ codeToMap c = codeToMap_r [] c
 
 h_encode code str = foldl (++) [] (map ((codeToMap code)!) str)
 
+representativeSample =
+	-- from start of ActiveShooter
+	"[2,0,0,\"n\",\"ActiveShooterDef\",{\"inProgress\":{\"pickUpBook\":\"Go pick up your book from the library on the 2nd floor.\"},\"completed\":{},\"failed\":{}},[\"UseDoor\",\"shooterR111\",\"bell-hiding\",\"HidingPlace\",\"doorShadeDown\",\"door-open\",\"door2-open\",\"lock1\",\"lock2\",\"doorShadeDown041\",\"hidingSpot\",\"hidingSpot2\",\"doorHandleDone\",\"lightSwitchDone\",\"hidingStudent1\",\"hidingStudent2\",\"hidingStudent3\",\"shooter220S\"],{},\"on\",[[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[0],-1],[[1],-1],[[1],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[0],-1],[[0],-1],[[1],-1],[[1],-1],[[0],-1],[[0],-1],[[0],-1],[[0],-1],[[0],-1],[[0],-1],[[1],-1],[[0],-1],[[1],-1],[[1],-1],[[]],[[1],-1],[[0],-1],[[1],-1],[[0],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[0],-1],[[0],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[0],-1],[[1],-1],[[0],-1],[[0],-1],[[1],-1],[[0],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[0],-1],[[1],-1],[[1],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[0],-1],[[0],-1],[[1],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1]]]" ++
+	-- from start of Earthquake, after packing go-kit
+	"[0,0,2,\"w\",\"EarthquakeDef\",{\"inProgress\":{\"travelToSchool\":\"Exit your room to go to school.\"},\"completed\":{\"packForSchool\":\"Pack your go-kit. Then head to school\"},\"failed\":{}},[\"heavy-on-floor\",\"Movedwire\",\"bridgeDoor\",\"bikeMoved\",\"beam\",\"west-exit\",\"door-open\",\"door2-open\",\"Luke2\",\"mask\",\"whistle\",\"flashlight\",\"food\",\"water\",\"firstaidkit\"],{\"Breathing Mask\":{\"name\":\"Breathing Mask\",\"image\":\"breathingmask.png\",\"width\":32,\"height\":32},\"Whistle\":{\"name\":\"Whistle\",\"image\":\"whistle.png\",\"width\":32,\"height\":32},\"Flashlight\":{\"name\":\"Flashlight\",\"image\":\"flashlight.png\",\"width\":32,\"height\":32},\"Food\":{\"name\":\"Food\",\"image\":\"food.png\",\"width\":32,\"height\":32},\"Water\":{\"name\":\"Water\",\"image\":\"waterbottle.png\",\"width\":32,\"height\":32},\"First Aid Kit\":{\"name\":\"First Aid Kit\",\"image\":\"firstaidkit.png\",\"width\":32,\"height\":32}},\"on\",[[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],2],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[]],[[1],-1],[[1],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[0],-1],[[1],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[]],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[0],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[]],[[0],-1],[[1],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],10],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[]],[[]],[[]],[[]],[[1],-1],[[]],[[]],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[0],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1],[[1],-1]]]" ++
+	-- make sure all characters can be saved
+	[' '..'~']
+
+toJSON l = "{" ++ intercalate "," (map (\(k,v) -> show k ++ ":" ++ show v) l) ++ "}"
+toChar True = '1'
+toChar False = '0'
+
+emergencyResponseCompression =
+	let code = map (\(ch,co) -> ([ch], map toChar co)) $
+		Map.toList $ codeToMap $ createCode representativeSample
+	in putStrLn $ "var hencode = " ++ toJSON code ++
+		";\nvar hdecode = " ++ toJSON (map (\(x,y)->(y,x)) code) ++ ";"
