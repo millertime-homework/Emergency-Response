8211b782a997df6b9f37a9cee637ec8556043dd1
rf11707@gmail.com <rf11707@gmail.com>
2013-03-16 21:26:44 -0700

    Fixed objective transition when leaving the apartment.

--- scenarios/Earthquake/triggers/doorToSchool.js
+++ scenarios/Earthquake/triggers/doorToSchool.js
@@ -1,7 +1,6 @@
 'doorToSchool': {
     'events': {
         'player-move': ['move-forward'],
-        'completeObjective': ['travelToSchool'],
-        'setObjective':['getToClass','Make your way to room 106. Class has almost begun.']   
+        'completeObjective': ['travelToSchool'] 
     }
 }
