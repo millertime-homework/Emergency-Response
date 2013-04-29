2316f0b05245d524f224900a8a74972012f78d1f
Russell <compsciwizkid@gmail.com>
2013-04-28 22:48:16 -0700

    made the after earthquake conversation an action

--- scenarios/Earthquake/triggers/checkIfSurvivedEarthquake.js
+++ scenarios/Earthquake/triggers/checkIfSurvivedEarthquake.js
@@ -3,6 +3,6 @@
     'events': {
         'completeObjective': ['surviveEarthquake'],
         'setObjective': ['evacuateSchool', 'Evacuate the School safely'],
-        'showConversation': ['Survived Earthquake']
+        'showConversation': ['Survived Earthquake', 1, true, true]
     }
 }
