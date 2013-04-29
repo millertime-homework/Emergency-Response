96dfc9ec7bb4a5dcde62915f76506f6dd50a883a
Russell <compsciwizkid@gmail.com>
2013-04-28 19:24:36 -0700

    merged chalk triggers into one file

--- scenarios/Earthquake/triggers/getChalkObjective.js
+++
@@ -1,6 +0,0 @@
-'getChalkObjective': {
-    'events': {
-        'completeObjective': ['getToClass'],
-        'setObjective': ['getChalkForTeacher', 'Get the chalk from the sturdy desk.']
-    }
-}
--- scenarios/Earthquake/triggers/takeChalk.js
+++
@@ -1,6 +0,0 @@
-'takeChalk': {
-    'events': {
-        'takeFromScene' : [ {'name': 'Chalk', 'image': 'chalk.png', 'width':32, 'height':32 },
-                            'chalk' ]
-    }
-}
