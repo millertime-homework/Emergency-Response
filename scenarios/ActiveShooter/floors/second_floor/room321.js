e9d59ab96091f939d671dd61519885a09178fd43
Russell <compsciwizkid@gmail.com>
2013-05-16 21:58:34 -0700

    fixed R321 Active Shooter props

--- scenarios/ActiveShooter/floors/second_floor/room321.js
+++ scenarios/ActiveShooter/floors/second_floor/room321.js
@@ -7,28 +7,24 @@
             'name': 'EHall321',
             'image': 'R321-east.jpg'
         },
-        'w': {
-            'name': 'WHall321',
-            'image': 'R321-west.jpg'
-        },
         'n': {
             'name': 'NHall321',
             'image': 'R321-north.jpg',
-            '_props' : {
+            '_props': {
                 'lockedDoor': {
                     'name': 'lockedDoor',
                     'image': 'door.png',
                     'hoverImage': 'doorHover.png',
-                    'width': 550,
-                    'height': 500,
-                    'left': 220,
-                    'top': 150,
+                    'width': 282,
+                    'height': 510,
+                    'top': 143,
+                    'left': 418,
+                    'barrier': true,
                     'action': 'showConversation',
                     'actionVariables': {
                         'conversationName': 'lockedDoor',
                         'isAnAction': true
-                    },
-                'barrier' : true
+                    }
                 }
             }
         },
@@ -38,6 +34,10 @@
             'destination': {
                 'y': 1
             }
+        },
+        'w': {
+            'name': 'WHall321',
+            'image': 'R321-west.jpg'
         }
     }
 }
