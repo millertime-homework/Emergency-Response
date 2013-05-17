b4da7eec507706df6fea4dc47aac15adcc850ab6
Russell <compsciwizkid@gmail.com>
2013-05-16 21:40:18 -0700

    fixed R121 Active Shooter props

--- scenarios/ActiveShooter/floors/second_floor/room121.js
+++ scenarios/ActiveShooter/floors/second_floor/room121.js
@@ -3,7 +3,7 @@
     'x': 1,
     'y': 2,
     'annotation': 'AnnotationStairs.png',
-    '_triggers': ['chanceofEscape', 'deathIn240', 'getShotAtYopOfStairs'],
+    '_triggers': ['chanceofEscape','deathIn240','getShotAtYopOfStairs'],
     '_walls': {
         'e': {
             'name': 'EHall121',
@@ -18,29 +18,8 @@
                     'image': 'shooter.png',
                     'width': 61,
                     'height': 175,
-                    'left': 520,
-                    'top': 366
-                }
-            }
-        },
-        'w': {
-            'name': 'WHall121',
-            'image': 'R121-west.jpg',
-            '_props' : {
-                'lockedDoor': {
-                    'name': 'lockedDoor',
-                    'image': 'door.png',
-                    'hoverImage': 'doorHover.png',
-                    'width': 235,
-                    'height': 450,
-                    'left': 450,
-                    'top': 170,
-                    'action': 'showConversation',
-                    'actionVariables': {
-                        'conversationName': 'lockedDoor',
-                        'isAnAction': true
-                    },
-                'barrier' : true
+                    'top': 366,
+                    'left': 520
                 }
             }
         },
@@ -58,8 +37,8 @@
                     'hoverImage': 'doorHover.png',
                     'width': 280,
                     'height': 500,
-                    'left': 420,
                     'top': 160,
+                    'left': 420,
                     'barrier': true,
                     'action': 'showConversation',
                     'actionVariables': {
@@ -81,8 +60,29 @@
                     'image': 'shooter.png',
                     'width': 32,
                     'height': 64,
-                    'left': 570,
-                    'top': 310
+                    'top': 310,
+                    'left': 570
+                }
+            }
+        },
+        'w': {
+            'name': 'WHall121',
+            'image': 'R121-west.jpg',
+            '_props': {
+                'lockedDoor': {
+                    'name': 'lockedDoor',
+                    'image': 'door.png',
+                    'hoverImage': 'doorHover.png',
+                    'width': 271,
+                    'height': 531,
+                    'top': 144,
+                    'left': 430,
+                    'barrier': true,
+                    'action': 'showConversation',
+                    'actionVariables': {
+                        'conversationName': 'lockedDoor',
+                        'isAnAction': true
+                    }
                 }
             }
         }
