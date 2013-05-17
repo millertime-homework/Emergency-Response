b523663e409b347a34e16fe57d016c06aba05f1d
Russell <compsciwizkid@gmail.com>
2013-05-16 22:26:08 -0700

    repositioned EQ apartment door prop

--- scenarios/Earthquake/floors/apartment_floor/apartment.js
+++ scenarios/Earthquake/floors/apartment_floor/apartment.js
@@ -2,9 +2,42 @@
     'id': 'apartment',
     'x': 0,
     'y': 0,
-    '_triggers' : ['hideFirstFloorRoomAnnotations', 'hideSecondFloorRoomAnnotations',
-                   'packForSchool', 'showAskInstruction'],
+    '_triggers': ['hideFirstFloorRoomAnnotations','hideSecondFloorRoomAnnotations','packForSchool','showAskInstruction'],
     '_walls': {
+        'e': {
+            'name': 'apartment-e',
+            'image': 'apartment-e.jpg',
+            '_props': {
+                'flashlight': {
+                    'name': 'Flashlight',
+                    'image': 'flashlight.png',
+                    'hoverImage': 'flashlightHover.png',
+                    'width': 62,
+                    'height': 40,
+                    'top': 420,
+                    'left': 400,
+                    'action': 'showConversation',
+                    'actionVariables': {
+                        'conversationName': 'packFlashlight',
+                        'isAnAction': true
+                    }
+                },
+                'food': {
+                    'name': 'Food',
+                    'image': 'food.png',
+                    'hoverImage': 'foodHover.png',
+                    'width': 60,
+                    'height': 130,
+                    'top': 350,
+                    'left': 700,
+                    'action': 'showConversation',
+                    'actionVariables': {
+                        'conversationName': 'packFood',
+                        'isAnAction': true
+                    }
+                }
+            }
+        },
         'n': {
             'name': 'apartment-n',
             'image': 'apartment-n.jpg',
@@ -42,58 +75,23 @@
         's': {
             'name': 'apartment-s',
             'image': 'apartment-s.jpg',
+            'destination': {
+                'x': 2,
+                'y': 0,
+                'z': 0
+            },
             '_props': {
                 'door': {
                     'name': 'Exit to School',
                     'image': 'door.png',
                     'hoverImage': 'doorHover.png',
-                    'top': 160,
-                    'left': 470,
-                    'width': 180,
-                    'height': 390,
+                    'width': 208,
+                    'height': 418,
+                    'top': 149,
+                    'left': 459,
                     'action': 'showConversation',
                     'actionVariables': {
                         'conversationName': 'travelToSchool',
-                        'isAnAction': true
-                    }
-                }
-            },
-            'destination': {
-                'x': 2,
-                'y': 0,
-                'z': 0,
-                'f': 'n'
-            }
-        },
-        'e': {
-            'name': 'apartment-e',
-            'image': 'apartment-e.jpg',
-            '_props': {
-                'food': {
-                    'name': 'Food',
-                    'image': 'food.png',
-                    'hoverImage': 'foodHover.png',
-                    'width': 60,
-                    'height': 130,
-                    'top': 350,
-                    'left': 700,
-                    'action': 'showConversation',
-                    'actionVariables': {
-                        'conversationName': 'packFood',
-                        'isAnAction': true
-                    }
-                },
-                'flashlight': {
-                    'name': 'Flashlight',
-                    'image': 'flashlight.png',
-                    'hoverImage': 'flashlightHover.png',
-                    'width': 62,
-                    'height': 40,
-                    'top': 420,
-                    'left': 400,
-                    'action': 'showConversation',
-                    'actionVariables': {
-                        'conversationName': 'packFlashlight',
                         'isAnAction': true
                     }
                 }
@@ -103,44 +101,44 @@
             'name': 'apartment-w',
             'image': 'apartment-w.jpg',
             '_props': {
-                'shelf0': {
-                    'name': 'Shelf 0',
-                    'image': 'woodshelf.png',
-                    'top': 400,
-                    'left': 130,
-                    'width': 260,
-                    'height': 53 
-                },
-                'shelf1': {
-                    'name': 'Shelf 1',
-                    'image': 'woodshelf.png',
-                    'top': 400,
-                    'left': 750,
-                    'width': 260,
-                    'height': 53 
-                },
                 'firstaidkit': {
                     'name': 'First Aid Kit',
                     'image': 'firstaidkit.png',
                     'hoverImage': 'firstaidkitHover.png',
-                    'top': 323,
-                    'left': 800,
                     'width': 124,
                     'height': 102,
+                    'top': 323,
+                    'left': 800,
                     'action': 'showConversation',
                     'actionVariables': {
                         'conversationName': 'packFirstAidKit',
                         'isAnAction': true
                     }
                 },
+                'shelf0': {
+                    'name': 'Shelf 0',
+                    'image': 'woodshelf.png',
+                    'width': 260,
+                    'height': 53,
+                    'top': 400,
+                    'left': 130
+                },
+                'shelf1': {
+                    'name': 'Shelf 1',
+                    'image': 'woodshelf.png',
+                    'width': 260,
+                    'height': 53,
+                    'top': 400,
+                    'left': 750
+                },
                 'water': {
                     'name': 'Water Bottle',
                     'image': 'waterbottle.png',
                     'hoverImage': 'waterbottleHover.png',
-                    'top': 265,
-                    'left': 220,
                     'width': 75,
                     'height': 140,
+                    'top': 265,
+                    'left': 220,
                     'action': 'showConversation',
                     'actionVariables': {
                         'conversationName': 'packWater',
