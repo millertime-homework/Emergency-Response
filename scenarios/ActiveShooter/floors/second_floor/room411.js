09d7cbf3d063e509aefdc857e86b9fc711d92f1e
Russell <compsciwizkid@gmail.com>
2013-05-16 22:17:01 -0700

    fixed R411 Active Shooter props

--- scenarios/ActiveShooter/floors/second_floor/room411.js
+++ scenarios/ActiveShooter/floors/second_floor/room411.js
@@ -8,6 +8,54 @@
             'name': 'ERoom411',
             'image': 'R411-east.jpg'
         },
+        'entryScene': {
+            'name': 'WRoom411',
+            'image': 'R411-west.jpg',
+            'fakeDirection': 'w',
+            'isCutscene': true,
+            '_props': {
+                'group-of-people': {
+                    'name': 'group-of-people',
+                    'image': 'GroupOfPeople.png',
+                    'width': 199,
+                    'height': 249,
+                    'top': 230,
+                    'left': 600
+                }
+            }
+        },
+        'n': {
+            'name': 'NRoom411',
+            'image': 'R411-north.jpg'
+        },
+        's': {
+            'name': 'SRoom411',
+            'image': 'R411-south.jpg'
+        },
+        'shooterEntersScene': {
+            'name': 'WRoom411',
+            'image': 'R411-west.jpg',
+            'fakeDirection': 'w',
+            'isCutscene': true,
+            '_props': {
+                'da-shootah': {
+                    'name': 'da-shootah',
+                    'image': 'shooter.png',
+                    'width': 74,
+                    'height': 226,
+                    'top': 269,
+                    'left': 488
+                },
+                'group-of-people-reacting': {
+                    'name': 'group-of-people-reacting',
+                    'image': 'GroupOfPeopleReacting.png',
+                    'width': 172,
+                    'height': 265,
+                    'top': 289,
+                    'left': 592
+                }
+            }
+        },
         'w': {
             'name': 'WRoom411',
             'image': 'R411-west.jpg',
@@ -20,64 +68,16 @@
                     'image': 'door.png',
                     'openImage': 'doorOpen.png',
                     'hoverImage': 'doorHover.png',
-                    'width': 140,
-                    'height': 240,
-                    'left': 500,
-                    'top': 250,
+                    'width': 99,
+                    'height': 206,
+                    'top': 248,
+                    'left': 509,
                     'barrier': true,
                     'action': 'showConversation',
                     'actionVariables': {
                         'conversationName': 'door',
                         'isAnAction': true
                     }
-                }
-            }
-        },
-        'n': {
-            'name': 'NRoom411',
-            'image': 'R411-north.jpg'
-        },
-        's': {
-            'name': 'SRoom411',
-            'image': 'R411-south.jpg'
-        },
-        'entryScene': {
-            'name': 'WRoom411',
-            'image': 'R411-west.jpg',
-            'fakeDirection' : 'w',
-            'isCutscene': true,
-            '_props': {
-                'group-of-people': {
-                    'name': 'group-of-people',
-                    'image': 'GroupOfPeople.png',
-                    'width': 199,
-                    'height': 249,
-                    'left': 600,
-                    'top': 230
-                }
-            }
-        },
-        'shooterEntersScene': {
-            'name': 'WRoom411',
-            'image': 'R411-west.jpg',
-            'fakeDirection' : 'w',
-            'isCutscene': true,
-            '_props': {
-                'group-of-people-reacting': {
-                    'name': 'group-of-people-reacting',
-                    'image': 'GroupOfPeopleReacting.png',
-                    'width': 172,
-                    'height': 265,
-                    'left': 600,
-                    'top': 230
-                },
-                'da-shootah': {
-                    'name': 'da-shootah',
-                    'image': 'shooter.png',
-                    'width': 90,
-                    'height': 256,
-                    'left': 460,
-                    'top': 250
                 }
             }
         }
